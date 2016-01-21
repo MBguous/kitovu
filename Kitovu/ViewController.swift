481f7cb9fdaae55fe6b9173c6372513115e9543c
Mahesh <mahesh@Maheshs-MacBook-Pro.local>
2016-01-21 16:10:51 +0545

    first commit

--- Kitovu/ViewController.swift
+++ Kitovu/ViewController.swift
@@ -6,13 +6,34 @@
 //  Copyright © 2016 Techno Troop. All rights reserved.
 //
 
+
 import UIKit
+import QuartzCore
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signIn: UIButton!
+    @IBOutlet weak var facebook: UIButton!
+    @IBOutlet weak var google: UIButton!
+    @IBOutlet weak var twitter: UIButton!
+    
+    @IBOutlet weak var register: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.signIn.layer.cornerRadius = 10.0
+        
+        self.facebook.layer.cornerRadius = 15
+        
+        self.google.layer.cornerRadius = 15
+        
+        self.twitter.layer.cornerRadius = 15
+        
+        self.register.layer.cornerRadius = 10
+        
     }
 
     override func didReceiveMemoryWarning() {
